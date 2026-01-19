7758ee044a94d0d68d497180d62f6af66a0b49e0
NGUYEN DUC DAT <nguyenducdat0406@gmail.com>
2026-01-19 21:40:01 +0700

    Initial project structure and base classes
    Add solution and projects for API, Core, and Infrastructure layers. Implement base DTOs, interfaces, and abstract classes for repository and service patterns. Include middleware for global exception handling, helper utilities, and configuration for dependency injection, CORS, and Swagger. Project is set up for .NET 8 with initial models and settings.

---
+++ Api/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Core.Interface.Repo;
+using Core.Interface.Service;
+using Core.Middleware;
+using Core.Service;
+using Infra.Repo;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Encodings.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Cấu Hình DI:
+builder.Services.AddControllers();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Cấu hình JSON để không escape ký tự Unicode
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Encoder =
+        JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+});
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Frontend URL
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
+
+var app = builder.Build();
+
+// *** QUAN TRỌNG: Đăng ký middleware xử lý exception toàn cục ***
+// Phải đặt đầu tiên trong pipeline để bắt được tất cả exception
+app.UseMiddleware<HandleExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseCors("AllowFrontend");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Core/DTO/General/SaveDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Core.DTO.General
+{
+    public class SaveDTO<T>
+    {
+        public int State { get; set; }   // 1=Create, 2=Update, 4=Duplicate
+        public int Mode { get; set; }
+
+        public required T EntityDTO { get; set; }
+    }
+
+}
