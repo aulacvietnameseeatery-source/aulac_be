be12f4bc8d57bc00a9106f63a33408f454cd65a1
HaloicCa <hungnvhe187005@fpt.edu.vn>
2026-03-06 05:07:12 +0700

    ingredient management, stock history

---
+++ Infa/Data/Migrations/20260305205500_AddMinStockLevelToCurrentStock.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infa.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMinStockLevelToCurrentStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "staff_id",
+                table: "orders",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MinStockLevel",
+                table: "current_stock",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinStockLevel",
+                table: "current_stock");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "staff_id",
+                table: "orders",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
